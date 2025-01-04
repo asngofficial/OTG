15eda2a3a1d0a940479bf00c44294bae53be46af
asngofficial <samuelafon@my.unt.edu>
2025-01-03 19:58:50 -0600

    fixed menu bar on all pages

--- JS/otg-script.js
+++
@@ -1,6 +0,0 @@
-<script>
-
-    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
-    const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
-
-</script>
